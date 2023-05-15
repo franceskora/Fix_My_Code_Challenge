ec30693365cd126c3670d8de0e32d5f345864210
franceskora <francesanyakora@gmail.com>
2023-05-15 15:13:56 -0700

    my commit

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -6,7 +6,7 @@ class square():
     height = 0
 
 
-    def __init__(self, *args, *kwargs):
+    def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
